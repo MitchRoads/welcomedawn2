41220e2424af426cccd7bd5434250c1b1306a64f
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-11-19 13:28:48 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -18,7 +18,8 @@ client.on('message', async (message, member) => {
 	   let user = message.mentions.users.first(); 
 if(!user) return message.channel.send("You haven't selected/mentioned a user whose avatar you want to see."); 
     let avatarEmbed = new Discord.RichEmbed()
-    .setAuthor(`${user.username}'s Profile Picture`)
+    .setTitle(`${user.username}'s Profile Picture`)
+    .setURL(user.displayAvatarURL)
     .setImage(user.displayAvatarURL)
     .setColor("#ea9b67")
     .setTimestamp();
