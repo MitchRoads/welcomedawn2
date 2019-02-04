a0c945ec3fc7c14dfd6011fcfce225eed001852f
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2019-02-03 21:03:17 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -42,7 +42,7 @@ if(!user) return message.channel.send("You haven't selected/mentioned a user who
     .addField("Date Of Creation", client.user.createdAt.toLocaleDateString(), true)
     .addField("Last Update", `${moment.duration(client.uptime).format('d[d ]h[h ]m[m ]s[s]')} ago`, true)
     .addField("Discord.js Version", "discord.js 11.4.2", true)
-    .addField("Dawn Public Server", "https://discord.gg/wz4NnZk", true)
+//     .addField("Dawn Public Server", "https://discord.gg/wz4NnZk", true)
     .setFooter("Created By @Dawn.Bots.INC", client.user.displayAvatarURL)
     .setTimestamp();
     return message.channel.send(botembed);
@@ -55,10 +55,13 @@ client.on('guildMemberAdd', (member) => {
   let server = member.guild.name;
 member.addRole(`456990891987697675`);
   let welcome = guild.channels.find(c => c.name === 'welcome');
+  let membericon = member.user.displayAvatarURL; 
   let gembed = new Discord.RichEmbed()
-      .setTitle("User Enterance")
+      .setTitle(`User Enterance: ${member.user.tag}`)
       .setColor("#000000")
       .setDescription(`Welcome ${member}, to **${server}**, hope you enjoy your stay.`)
+      .setFooter(`USER ID: ${member.id}`, member.displayAvatarURL)
+      .setThumbnail(membericon)
       .setTimestamp()
   welcome.send(gembed);
 	      });
@@ -67,11 +70,14 @@ client.on('guildMemberRemove', (member, message) => {
 	
   let guild = member.guild;
   let server = member.guild.name;
-  let logs = guild.channels.find(c => c.name === 'logs');
+  let logs = guild.channels.find(c => c.name === 'logs')
+  let membericon = member.user.displayAvatarURL; 
   let gembed = new Discord.RichEmbed()
-      .setTitle("User Departure")
+      .setTitle(`User Departure: ${member.user.tag}`)
       .setColor("#000000")
       .setDescription(`Too bad that ${member} has decided to go, maybe one day you'll return to us. But for now, au revoir.`)
+     .setFooter(`USER ID: ${member.id}`, member.displayAvatarURL)
+      .setThumbnail(membericon)
       .setTimestamp()
   logs.send(gembed);
 	      });	
